4555b05c613a0bfc8e0d970ce483fa4ef18861f6
Рифат Бахтиев <rifatbakhtiev@gmail.com>
2022-12-20 23:54:00 +0400

    Secound commit

--- Join.sql
+++ Join.sql
@@ -1,11 +1,42 @@
-select * from genre
-cross join singer;
-
-select album.name, track.name from album
-left join track on album.id=track.album_id;
-
+--1
 select genre.name, count(singer.name)  from genre
 left join genresinger on genre.id=genresinger.genre_id
 left join singer on genresinger.singer_id=singer.id
 group by genre.name;
+
+--2
+select album.name, count(track.name)  from album
+left join track on album.id=track.album_id
+where album.year_of_issue between '1990-01-01' and '2021-01-01'
+group by album.name;
+
+--3
+select album.name, avg(track.duration)  from album
+left join track on album.id=track.album_id
+group by album.name;
+
+--4
+select singer.name from singer
+left join singeralbum on singer.id=singeralbum.singer_id
+left join album on singeralbum.album_id=album.id
+where album.year_of_issue not between '2020-01-01' and '2020-12-31'
+group by singer.name;
+
+--5
+select compilation.name from compilation
+left join trackcompilation on compilation.id=trackcompilation.compilation_id
+left join track on trackcompilation.track_id=track.id
+left join album on track.album_id=album.id
+left join singeralbum on album.id=singeralbum.album_id
+left join singer on singeralbum.singer_id=singer.id
+where singer.name like '%ДДТ%'
+group by compilation.name;
+
+--6
+select album.name, count(singer.name) from album
+left join singeralbum on album.id=singeralbum.album_id
+left join singer on singeralbum.singer_id=singer.id
+left join genresinger on singer.id=genresinger.singer_id
+left join genre on genresinger.genre_id=genre.id
+group by album.name;
 
